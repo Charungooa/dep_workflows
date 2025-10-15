ab887f62a437a172749257b6fc3e538f9e78737f
Amit Charungoo <v-2amitc@hotmail.com>
2025-10-15 07:20:36 +0530

    updates the project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Web App</h1>
+        <p>Successfully deployed to Azure Kubernetes Service!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
